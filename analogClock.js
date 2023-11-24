2a2a1628d70b66279212db3b79615b8aa948d18d
Jonas Svay <svay.jonas6@gmail.com>
2023-11-24 15:06:28 +0100

    improvements

--- analogClock.js
+++ analogClock.js
@@ -1,13 +1,18 @@
-const secHand = document.querySelector('.second-hand');
-const minHand = document.querySelector('.min-hand');
+//Storing the Elements in their variable: hourHand,minHand and secHand
 const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
+const secHand = document.querySelector('.second-hand');
 
+//Function with the name "setDate"
 function setDate(){
+
+    // Get the current time (Date,Hour,Minute and Second)
     const now = new Date();
     const hours = now.getHours();
     const mins = now.getMinutes();
     const seconds = now.getSeconds();
     
+    // Calculate rotation degrees for each clock hand
     const hourDegrees = ((hours / 12) * 360) + ((mins/60)*30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
@@ -17,33 +22,31 @@ function setDate(){
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secHand.style.transform = `rotate(${secondsDegrees}deg)`;
 }
+
+//Excute "setDate" with fixed time intervals
 setInterval(setDate, 1000);
 
+//Call the function "setDate()"
 setDate();
 
-
+// Get the current time (date, dour, minute and second)
 const now = new Date();
 const hours = now.getHours();
 const mins = now.getMinutes();
 const seconds = now.getSeconds();
 
+//Write in the HTML-Document
 document.write(`<p>Zeit in Zürich: ${now}</p>`);
-setInterval(document.write, 1000);
 
-
-const newYorkTimeZone = 'America/New_York';
-
-// Optionen für das Datumsformat
+//Create "options" object and formatting hour, minute and second.
 const options = {
-    timeZone: newYorkTimeZone,
-    hour12: false, // 24-Stunden-Format
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric'
 };
 
-// Aktuelle Zeit von New York abrufen und formatieren
+//Formatting the date for New York
 const newYorkTime = new Date().toLocaleString('en-US', options);
 
-// Anzeige der Zeit von New York im HTML-Dokument
+//Write in the HTML-Document
 document.write(`<p>Aktuelle Zeit in New York: ${newYorkTime}</p>`);
